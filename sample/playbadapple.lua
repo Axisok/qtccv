6e51e62f3343bd2800346f75fef720aa1820ad40
Diogo Yoshiura Moreira <axisokk@gmail.com>
2021-08-18 20:32:35 -0300

    bad apple
    bad apple.

--- sample/playbadapple.lua
+++ sample/playbadapple.lua
@@ -1,3 +1,21 @@
 -- Program to download and play bad apple on loop.
 
--- Will be made shortly.
+local function getFile(name, lname)
+	local r = http.get("https://raw.githubusercontent.com/Axisok/qtccv/master/" .. name)
+	local f = fs.open(lname, "w")
+	f.write(r.readAll())
+	f.close()
+	
+end
+
+if (not fs.exists("badapple.qtv")) then
+	getFile("decode/apis/bitreader.lua", "apis/bitreader.lua")
+	getFile("decode/apis/hexscreen.lua", "apis/hexscreen.lua")
+	getFile("decode/apis/wave.lua", "apis/wave.lua")
+	getFile("decode/videoplayer.lua", "videoplayer.lua")
+	getFile("sample/badapple.nbs", "badapple.nbs")
+	getFile("sample/badapple.qtv", "badapple.qtv")
+	
+end
+
+shell.run("videoplayer", "badapple")
